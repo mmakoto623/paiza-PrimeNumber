68ad68a471252462cdff7a287d30d8c6456d5ce7
mmakoto623 <46061108+mmakoto623@users.noreply.github.com>
2021-10-02 00:02:41 +0900

    Create STEP2_素数判定.js

---
+++ STEP2_素数判定.js
@@ -0,0 +1,34 @@
+//素数かどうか判定する
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  let N = parseInt(lines[0]);
+  let flg = true;//true 素数　false 素数でない
+  if((N%2 === 0 && N > 2) || (N === 1)){
+      flg = false;
+  }
+  
+  for(let i= 3;i<Math.sqrt(N)+1;i+=2){
+      if(N%i ===0 ){
+          flg = false;
+          break;
+      }
+  }
+  
+  if(flg === true) {
+      console.log("YES");
+  } else {
+      console.log("NO");
+  }
+});
+
